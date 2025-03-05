de0149ea0d4593ff83d88b25e071440530ae3762
JakubKyhos <kubakyhos4@gmail.com>
2025-03-05 12:36:23 +0100

    add password basic func

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
